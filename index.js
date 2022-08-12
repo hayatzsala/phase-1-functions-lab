b09168ed4488eef0004762776310c704ce680dae
hayatSalawdeh <salawdeh.hayat@gmail.com>
2022-08-13 02:00:32 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const refStreet = 42;
+function distanceFromHqInBlocks(num){
+    return Math.abs(refStreet - num);
+}
+
+function distanceFromHqInFeet(num){
+    return (Math.abs(refStreet - num)*264);
+
+}
+function distanceTravelledInFeet(d1,d2){
+    return (Math.abs(d1 - d2)*264);
+}
+function calculatesFarePrice(start,dst){
+    const distance = (Math.abs(start - dst)*264);
+    let price =0;
+    if(distance <= 400){
+        return 0;
+    }
+    else if(distance>400 && distance<2000){
+        return 0.02 * (distance-400);
+    }
+    else if(distance >=2000 && distance < 2500){
+        return 25;
+    }
+    else {
+        return "cannot travel that far";
+    }
+}
